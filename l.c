2f71447821db2fc91151e2b3905d6eb6d5695f6a
NandhiniManian <30834130+NandhiniManian@users.noreply.github.com>
2017-09-17 18:29:39 +0530

    Create l.c

---
+++ l.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{int i;
+printf("Enter the year:");
+scanf("%d",&i);
+if(i!=100&&i%4==0||i%400==0)
+{
+printf("%d is leap year",i);
+}
+else
+{
+printf("%d is not a leap year",i);
+}
+}
